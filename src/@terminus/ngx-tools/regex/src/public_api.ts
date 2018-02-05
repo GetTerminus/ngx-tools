b5591ffbd8af08ded41725701d68b610c78cfb70
Benjamin Charity <ben@benjamincharity.com>
2018-02-05 13:50:21 -0500

    added couple files, fix circle path for dist

--- src/@terminus/ngx-tools/regex/src/public_api.ts
+++ src/@terminus/ngx-tools/regex/src/public_api.ts
@@ -1,2 +1,3 @@
 export * from './email/email.regex';
 export * from './password/password.regex';
+export * from './credit-card/credit-card.regex';
--- src/@terminus/ngx-tools/utilities/src/public_api.ts
+++ src/@terminus/ngx-tools/utilities/src/public_api.ts
@@ -1,2 +1,3 @@
 export * from './noop/noop';
 export * from './debounce/debounce';
+export * from './group-by/group-by';
