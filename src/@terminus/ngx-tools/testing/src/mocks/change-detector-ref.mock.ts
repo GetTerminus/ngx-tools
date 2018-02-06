8206ffc9bd234379ee87de8597e6025814c200f7
Benjamin Charity <ben@benjamincharity.com>
2018-02-06 15:00:22 -0500

    remove jasmine helpers

--- src/@terminus/ngx-tools/testing/src/mocks/change-detector-ref.mock.ts
+++ src/@terminus/ngx-tools/testing/src/mocks/change-detector-ref.mock.ts
@@ -3,15 +3,9 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ChangeDetectorRefMock {
-
-  markForCheck = jasmine.createSpy('markForCheck');
-
-  detach = jasmine.createSpy('detach');
-
-  detectChanges = jasmine.createSpy('detectChanges');
-
-  checkNoChanges = jasmine.createSpy('checkNoChanges');
-
-  reattach = jasmine.createSpy('reattach');
-
+  markForCheck = () => {};
+  detach = () => {};
+  detectChanges = () => {};
+  checkNoChanges = () => {};
+  reattach = () => {};
 }
