7645734b41472c2fb81682a9084e481e19597e7d
Benjamin Charity <ben@benjamincharity.com>
2018-02-07 08:57:54 -0500

    fix: rename public_api to public-api, add tests for dispatch-events

--- src/@terminus/ngx-tools/testing/src/utilities/dispatch-events.spec.ts
+++ src/@terminus/ngx-tools/testing/src/utilities/dispatch-events.spec.ts
@@ -1,31 +1,96 @@
 import {
   dispatchEvent,
+  dispatchFakeEvent,
   /*
-   *dispatchFakeEvent,
    *dispatchKeyboardEvent,
-   *dispatchMouseEvent,
-   *dispatchTouchEvent,
    */
+  dispatchMouseEvent,
+  dispatchTouchEvent,
 } from './dispatch-events';
 import {
   createFakeEvent,
 } from './event-objects';
-import { createNodeMock } from './create-node-mock';
 /*
- *import * as KEYCODES from '@terminus/ngx-tools/keycodes';
+ *import * as KEYCODES from './../../../keycodes/src/public-api';
  */
 
 
-describe.only(`dispatchEvent`, () => {
+describe(`dispatch-events`, () => {
+  let eventMock;
+  let nodeMock;
 
-  test(`should trigger the dispatch and return the event`, () => {
-    const nodeMock = createNodeMock('div', createNodeMock('html', undefined));
-    nodeMock.dispatchEvent = jest.fn().mockName('dispatchEvent');
-    const eventMock = createFakeEvent('keydown');
-
-    const actual = dispatchEvent(window, eventMock);
-
-    expect(actual).toEqual(eventMock);
+  beforeEach(() => {
+    eventMock = createFakeEvent('keydown');
+    nodeMock = window.document.createElement('div');
+    nodeMock.dispatchEvent = jest.fn();
   });
+
+  afterEach(() => {
+    nodeMock.dispatchEvent.mockClear();
+  });
+
+
+  describe(`dispatchEvent`, () => {
+
+    test(`should trigger the dispatch and return the event`, () => {
+      const actual = dispatchEvent(nodeMock, eventMock);
+
+      expect(actual).toEqual(eventMock);
+      expect(nodeMock.dispatchEvent).toHaveBeenCalled();
+    });
+
+  });
+
+
+  describe(`dispatchFakeEvent`, () => {
+
+    test(`should trigger the dispatch and return the event`, () => {
+      const actual = dispatchFakeEvent(nodeMock, eventMock);
+
+      expect(actual).toEqual(eventMock);
+      expect(nodeMock.dispatchEvent).toHaveBeenCalled();
+    });
+
+  });
+
+
+  // TODO: this test is causing Jest to hang
+/*
+ *  describe.only(`dispatchKeyboardEvent`, () => {
+ *
+ *    test(`should do something`, () => {
+ *      const actual = dispatchKeyboardEvent(nodeMock, 'keydown', KEYCODES.A);
+ *
+ *      expect(actual).toEqual(eventMock);
+ *      expect(nodeMock.dispatchEvent).toHaveBeenCalled();
+ *    });
+ *
+ *  });
+ */
+
+
+  describe(`dispatchMouseEvent`, () => {
+
+    test(`should trigger the dispatch and return the event`, () => {
+      const actual = dispatchMouseEvent(nodeMock, 'mousedown', 10, 10);
+
+      expect(actual).toEqual(eventMock);
+      expect(nodeMock.dispatchEvent).toHaveBeenCalled();
+    });
+
+  });
+
+
+  describe(`dispatchTouchEvent`, () => {
+
+    test(`should trigger the dispatch and return the event`, () => {
+      const actual = dispatchTouchEvent(nodeMock, 'touchstart');
+
+      expect(actual).toEqual(eventMock);
+      expect(nodeMock.dispatchEvent).toHaveBeenCalled();
+    });
+
+  });
+
 
 });
