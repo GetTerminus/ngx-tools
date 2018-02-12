8a4b50a3b6b8caee79dfcea74ba77a6a6126bf5b
Benjamin Charity <ben@benjamincharity.com>
2018-02-12 09:41:24 -0500

    fix(Utility): isBoolean now correctly tests new Boolean(true)

--- src/@terminus/ngx-tools/src/verify-types/is-boolean/is-boolean.spec.ts
+++ src/@terminus/ngx-tools/src/verify-types/is-boolean/is-boolean.spec.ts
@@ -4,6 +4,9 @@ import { isBoolean } from './is-boolean';
 describe(`isBoolean`, () => {
   const bool1 = true;
   const bool2 = false;
+  // tslint:disable: no-construct
+  const bool3 = new Boolean(true);
+  // tslint:enable: no-construct
 
   const nonBool1 = null;
   const nonBool2 = undefined;
@@ -11,14 +14,12 @@ describe(`isBoolean`, () => {
   const nonBool4 = [];
   const nonBool5 = {};
   const nonBool6 = () => true;
-  // tslint:disable: no-construct
-  const nonBool7 = new Boolean(true);
-  // tslint:enable: no-construct
 
 
   test(`should return true for a boolean value`, () => {
     expect(isBoolean(bool1)).toEqual(true);
     expect(isBoolean(bool2)).toEqual(true);
+    expect(isBoolean(bool3)).toEqual(true);
   });
 
 
@@ -29,8 +30,6 @@ describe(`isBoolean`, () => {
     expect(isBoolean(nonBool4)).toEqual(false);
     expect(isBoolean(nonBool5)).toEqual(false);
     expect(isBoolean(nonBool6)).toEqual(false);
-    // NOTE: This test returns false for Boolean objects
-    expect(isBoolean(nonBool7)).toEqual(false);
   });
 
 });
--- src/@terminus/ngx-tools/src/verify-types/is-boolean/is-boolean.ts
+++ src/@terminus/ngx-tools/src/verify-types/is-boolean/is-boolean.ts
@@ -1,11 +1,10 @@
 /**
  * Determine if a value is a boolean
  *
- * NOTE: This test returns false for Boolean objects (eg `new Boolean(true)`)
- *
  * @param value - The value to test
  * @return The result
  */
 export function isBoolean(value: any): boolean {
-  return value === true || value === false || toString.call(value) === '[object Boolean]';
+  return value === true || value === false ||
+    Object.prototype.toString.call(value) === '[object Boolean]';
 }
