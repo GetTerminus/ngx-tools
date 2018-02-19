d69acf2473ad7cd979a1f771e2d0ec44e910e462
Benjamin Charity <ben@benjamincharity.com>
2018-02-19 17:22:14 -0500

    feat(Regex): add us phone regex

--- src/@terminus/ngx-tools/regex/src/public-api.ts
+++ src/@terminus/ngx-tools/regex/src/public-api.ts
@@ -3,4 +3,5 @@ export * from './email/email.regex';
 export * from './letters/letters.regex';
 export * from './numbers/numbers.regex';
 export * from './password/password.regex';
+export * from './phone/usa-phone.regex';
 export * from './url/url.regex';
