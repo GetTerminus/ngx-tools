360296e099483dbfc8c8f0e52cba4a0818da9145
Benjamin Charity <ben@benjamincharity.com>
2018-03-09 14:19:22 -0500

    fix(Testing): elementRef now contains nativeElement object with stubbed properties

--- src/@terminus/ngx-tools/testing/src/mocks/elementRef.mock.ts
+++ src/@terminus/ngx-tools/testing/src/mocks/elementRef.mock.ts
@@ -2,11 +2,21 @@ import { ElementRef } from '@angular/core';
 
 
 /**
+ * Default stubbed items for `nativeElement`
+ */
+const defaults = {
+  innerText: 'foo',
+  style: {},
+};
+
+
+/**
  * A mock of the Angular ElementRef class
  */
 export class ElementRefMock implements ElementRef {
-  nativeElement = {
-    innerText: 'foo',
-    style: {},
-  };
+  public nativeElement: any;
+
+  constructor(nativeElementAdditions: {[key: string]: any}) {
+    this.nativeElement = Object.assign({}, defaults, nativeElementAdditions);
+  }
 }
