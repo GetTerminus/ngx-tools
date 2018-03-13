9d4e67887b88a41931192bd9a66ed5b380ce2259
Benjamin Charity <ben@benjamincharity.com>
2018-03-13 08:55:07 -0400

    feat(Regex): add contains numbers regex and creator function

--- ngx-tools/regex/public-api.ts
+++ ngx-tools/regex/public-api.ts
@@ -4,6 +4,7 @@ export * from './src/letters/contains-lower.regex';
 export * from './src/letters/contains-upper.regex';
 export * from './src/letters/only-letters.regex';
 export * from './src/numbers/only-numbers.regex';
+export * from './src/numbers/contains-numbers.regex';
 export * from './src/password/password.regex';
 export * from './src/phone/usa-phone.regex';
 export * from './src/postal/usa-postal.regex';
--- ngx-tools/regex/src/letters/contains-lower.regex.ts
+++ ngx-tools/regex/src/letters/contains-lower.regex.ts
@@ -7,9 +7,14 @@ export const containsLowercaseRegex = /(.*[a-z].*)/;
 /**
  * Create a regex that requires a minimum amount of lowercase characters
  *
- * @param minLowercaseCount - The minimum amount of lowercase characters allowed
+ * @example
+ * const reg = createContainsLowercaseRegex(3);
+ * reg.test('aBC#1d') // Returns false
+ * reg.test('aBC#12D') // Returns true
+ *
+ * @param minimum - The minimum amount of lowercase characters required
  * @return The regex
  */
-export function createContainsLowercaseRegex(minLowercaseCount: number): RegExp {
-  return new RegExp(`(.*[a-z].*){${minLowercaseCount},}`)
+export function createContainsLowercaseRegex(minimum: number): RegExp {
+  return new RegExp(`(.*[a-z].*){${minimum},}`);
 }
