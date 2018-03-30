b1ec9e6e00f1122229d9f4725bc835e433497bf9
Benjamin Charity <ben@benjamincharity.com>
2018-03-30 09:48:04 -0400

    test(CI): now flagging test coverage for each module
    ISSUES CLOSED: #75

---
+++ ngx-tools/jest.keycodes.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  "globals": {
+    "ts-jest": {
+      "tsConfigFile": "./tsconfig.spec.json",
+      "enableTsDiagnostics": true
+    },
+    "__TRANSFORM_HTML__": true
+  },
+  "transform": {
+    "^.+\\.(ts|js|html)$": "./../node_modules/jest-preset-angular/preprocessor.js"
+  },
+  "testMatch": [
+    "<rootDir>/keycodes/**/?(*.)(spec|test).ts?(x)"
+  ],
+  "moduleFileExtensions": [
+    "ts",
+    "js",
+    "html"
+  ],
+  "transformIgnorePatterns": [
+    "./../node_modules/(?!@ngrx)"
+  ],
+  "clearMocks": true,
+  "collectCoverageFrom": [
+    "<rootDir>/keycodes/**/!(public-api|index).ts"
+  ],
+  "coverageDirectory": "<rootDir>/../coverage/keycodes/",
+  "setupTestFrameworkScriptFile": "./../tools/jest-setup.ts"
+}
