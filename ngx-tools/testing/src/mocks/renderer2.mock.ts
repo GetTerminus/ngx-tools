2e545967b916f80f49515e9acd6cac0490c96784
Benjamin Charity <ben@benjamincharity.com>
2018-05-10 08:33:48 -0400

    fix(Testing): add void implementations to Renderer2 mock
    ISSUES CLOSED: #164

--- ngx-tools/testing/src/mocks/renderer2.mock.ts
+++ ngx-tools/testing/src/mocks/renderer2.mock.ts
@@ -5,23 +5,25 @@ import { Renderer2 } from '@angular/core';
  * A mock of the Angular Renderer2
  */
 export class Renderer2Mock implements Renderer2 {
-  addClass;
-  appendChild;
-  createComment;
-  data;
-  destroy;
-  destroyNode;
-  insertBefore;
-  nextSibling;
-  parentNode;
-  removeAttribute;
-  removeChild;
-  removeClass;
-  removeStyle;
-  setAttribute;
-  setProperty;
-  setStyle;
-  setValue;
+  _data: any;
+  addClass() {};
+  appendChild() {};
+  createComment() {};
+  set data(v: any) {this._data = v};
+  get data(): any {return this._data};
+  destroy() {};
+  destroyNode() {};
+  insertBefore() {};
+  nextSibling() {};
+  parentNode() {};
+  removeAttribute() {};
+  removeChild() {};
+  removeClass() {};
+  removeStyle() {};
+  setAttribute() {};
+  setProperty() {};
+  setStyle() {};
+  setValue() {};
   animate() {}
   attachViewAfter() {}
   createElement() {}
