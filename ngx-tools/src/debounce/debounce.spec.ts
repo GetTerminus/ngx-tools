e3107b26bf25cd8e9e900db601936563aac63ad1
Benjamin Charity <ben@benjamincharity.com>
2018-05-18 14:22:50 -0400

    fix(Utility): debounce no longer loses this context

--- ngx-tools/src/debounce/debounce.spec.ts
+++ ngx-tools/src/debounce/debounce.spec.ts
@@ -1,20 +1,23 @@
-// tslint:disable: no-unused-variable
+import { TsWindowService } from './../window/window.service';
 import { debounce } from './debounce';
 
 
 describe(`debounce`, () => {
   let func: Function;
   let debounced: Function;
+  let windowMock: Window;
 
   beforeEach(() => {
     func = jest.fn();
     debounced = debounce(func, 200);
+    windowMock = new TsWindowService().nativeWindow;
   });
 
 
   test(`should debounce all calls for the 'wait' period`, (done) => {
     for (const i of [1, 2, 3]) {
-      debounced();
+      // NOTE: passing in `i` is not needed but TypeScript yells about the unused variable otherwise
+      debounced(i);
     }
 
     setTimeout(() => {
@@ -28,7 +31,8 @@ describe(`debounce`, () => {
     const TEST_DELAY = 210;
 
     for (const i of [1, 2, 3]) {
-      debounced();
+      // NOTE: passing in `i` is not needed but TypeScript yells about the unused variable otherwise
+      debounced(i);
     }
 
     setTimeout(() => {
@@ -39,6 +43,50 @@ describe(`debounce`, () => {
       expect(func).toHaveBeenCalledTimes(2);
       done();
     }, TEST_DELAY * 2);
+  });
+
+
+  test(`should fire immediately if specified`, (done) => {
+    const arr1: (string|number)[] = [];
+    const arr2: (string|number)[] = [];
+    const debouncedArr = [1, 2, 3, 4, 5, 'foo'];
+    const debouncedImmediateArr = [1, 'foo', 2, 3, 4, 5];
+    const origFunc = () => arr1.push('foo');
+    const origImmediateFunc = () => arr2.push('foo');
+    const debouncedOrig = debounce(origFunc, 200);
+    const debouncedImmediateOrig = debounce(origImmediateFunc, 200, true);
+
+    for (const v of [1, 2, 3, 4, 5]) {
+      arr1.push(v);
+      arr2.push(v);
+      debouncedOrig();
+      debouncedImmediateOrig();
+    }
+
+    setTimeout(() => {
+      expect(arr1).toEqual(debouncedArr);
+      expect(arr2).toEqual(debouncedImmediateArr);
+      done();
+    }, 1000);
+  });
+
+
+  test(`should use the passed in window reference`, (done) => {
+    jest.useFakeTimers();
+    const origFunc = () => {};
+    const debouncedOrig = debounce(origFunc, 200, false, windowMock);
+
+    for (const v of [1, 2, 3, 4, 5]) {
+      // NOTE: passing in `v` is not needed but TypeScript yells about the unused variable otherwise
+      debouncedOrig(v);
+    }
+
+    setTimeout(() => {
+      expect(setTimeout).toHaveBeenCalled();
+      done();
+    }, 400);
+
+    jest.runAllTimers();
   });
 
 });
