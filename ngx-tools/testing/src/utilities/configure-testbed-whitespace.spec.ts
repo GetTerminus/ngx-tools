e30ad7c2d51122258a0ab990f6e018a4b543544e
Benjamin Charity <ben@benjamincharity.com>
2018-05-25 10:53:01 -0400

    test(Testing): add test for configuring testbed whitespace

---
+++ ngx-tools/testing/src/utilities/configure-testbed-whitespace.spec.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+
+import {
+  ConfigureTestBedFn,
+  configureTestBedWhitespace,
+  TestBedCompilerOptions,
+} from './configure-testbed-whitespace';
+
+
+@Component({
+  template: ``,
+})
+class TestHostComponent {}
+
+
+describe(`configureTestBedWhitespace`, () => {
+  const configure: ConfigureTestBedFn = (testBed) => {
+    testBed.configureTestingModule({
+      declarations: [TestHostComponent],
+    });
+  };
+
+
+  test(`should return a configured TestBed`, () => {
+    configureTestBedWhitespace(configure).then((testBed) => {
+      expect(testBed).toEqual(expect.any(Function));
+      const bed = new testBed;
+      expect(bed.ngModule).toEqual(null);
+    });
+  });
+
+  test(`should return a configured TestBed`, () => {
+    const options: TestBedCompilerOptions = {
+      preserveWhitespaces: true,
+    };
+
+    configureTestBedWhitespace(configure, options).then((testBed) => {
+      expect(testBed).toEqual(expect.any(Function));
+      const bed = new testBed;
+      expect(bed.ngModule).toEqual(null);
+    });
+  });
+
+});
