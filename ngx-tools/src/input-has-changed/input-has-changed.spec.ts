8780cf5bc6f7bc2c6ad4e4978cd1ac0bd95a9fc8
Wendy Yang <atlwendy@gmail.com>
2018-07-19 11:45:11 -0400

    feat(updateControlOnChanges): allow deep key to be passed in

--- ngx-tools/src/input-has-changed/input-has-changed.spec.ts
+++ ngx-tools/src/input-has-changed/input-has-changed.spec.ts
@@ -12,16 +12,32 @@ describe(`inputHasChanged`, () => {
     item2: new SimpleChange('foo', 'foo', false),
   } as SimpleChanges;
 
+  describe(`with string input`, () => {
+    test(`should return undefined if the changes object or key are missing`, () => {
+      expect(inputHasChanged(null as any, 'foo')).toEqual(undefined);
+      expect(inputHasChanged(null as any, 'foo')).toEqual(undefined);
+    });
 
-  test(`should return undefined if the changes object or key are missing`, () => {
-    expect(inputHasChanged(null as any, 'foo')).toEqual(undefined);
-    expect(inputHasChanged(null as any, 'foo')).toEqual(undefined);
+
+    test(`should return true if the value has changed`, () => {
+      expect(inputHasChanged(changed, 'item1')).toEqual(true);
+      expect(inputHasChanged(changed, 'item2')).toEqual(false);
+    });
   });
 
+  describe(`with object input`, () => {
+    const changed2 = {
+      item1: new SimpleChange({ foo: 'bar' }, { foo: 'xyz' }, false),
+      item2: new SimpleChange({ foo: 'bar' }, { foo: 'bar' }, false),
+    } as SimpleChanges;
 
-  test(`should return true if the value has changed`, () => {
-    expect(inputHasChanged(changed, 'item1')).toEqual(true);
-    expect(inputHasChanged(changed, 'item2')).toEqual(false);
+    test(`should return true if the actual value has changed`, () => {
+      expect(inputHasChanged(changed2, 'item1.foo')).toEqual(true);
+    });
+
+    test(`should return false if the actual value has not changed`, () => {
+      expect(inputHasChanged(changed2, 'item2.foo')).toEqual(false);
+    });
   });
 
 });
--- ngx-tools/src/update-control-on-input-changes/update-control-on-input-changes.ts
+++ ngx-tools/src/update-control-on-input-changes/update-control-on-input-changes.ts
@@ -2,29 +2,32 @@ import { SimpleChanges } from '@angular/core';
 import { AbstractControl } from '@angular/forms';
 
 import { inputHasChanged } from './../input-has-changed/input-has-changed';
+import { NgChangeObjectValueParser } from '../utilities/ngchange-object-value-parser';
 
 
 /**
  * Helper function to determine if a specific value has changed
  *
  * @param changes - The object of changes
- * @param key - The object key in question
+ * @param path - A string with keys defined, separate with '.'
  * @param control - The formControl
  * @return True if the value has changed
  */
 export function updateControlOnInputChanges(
   changes: SimpleChanges,
-  key: string,
+  path: string,
   control: AbstractControl | null,
 ): boolean {
-  if (!changes || !key || !control) {
+  if (!changes || !path || !control) {
     return false;
   }
 
-  if (inputHasChanged(changes, key)) {
-    control.setValue(changes[key].currentValue);
+  if (inputHasChanged(changes, path)) {
+    const newValue = NgChangeObjectValueParser.getNewValue(changes, path);
+    control.setValue(newValue);
     return true;
   } else {
     return false;
   }
 }
+
