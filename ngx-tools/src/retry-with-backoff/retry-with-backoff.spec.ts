fd30ac27d3d7dd4a6e855dd9a20771180cd14d90
Benjamin Charity <ben@benjamincharity.com>
2018-08-24 12:02:23 -0400

    revert(Utility): "fix(Utility): update to work for simple paths"
    This reverts commit 7566904208091cc419f7b0aab8f7454f32c2f3a3.

--- ngx-tools/src/retry-with-backoff/retry-with-backoff.spec.ts
+++ ngx-tools/src/retry-with-backoff/retry-with-backoff.spec.ts
@@ -38,7 +38,7 @@ describe(`retryWithBackoff`, () => {
   });
 
 
-  test(`should retry for x retries and then throw`, (done) => {
+  test.only(`should retry for x retries and then throw`, (done) => {
     const error = new Error('bar');
     const seenValues: {[idx: number]: number} = {};
 
--- ngx-tools/src/utilities/ngchange-object-value-parser.ts
+++ ngx-tools/src/utilities/ngchange-object-value-parser.ts
@@ -1,51 +1,44 @@
 import { SimpleChanges } from '@angular/core';
 import { objectDeepParse } from './object-deep-parse';
 
-
 export class NgChangeObjectValueParser {
+
   /**
    * Function to parse previousValue from triggered by changes on ngOnChange
-   *
    * @param changes - SimpleChanges
    * @param path - string
    * @return lowest layer value or changes object itself when path cannot be parsed
    */
-  static getOldValue<T>(changes: SimpleChanges, path: string): T | undefined {
-    if (!path) {
-      return;
-    }
-    const [keys, key] = this.parsePath(path);
 
-    if (key && keys.length < 1) {
-      return changes[key] ? changes[key].previousValue : undefined;
-    } else {
-      return (key && changes[key]) ? objectDeepParse(changes[key].previousValue, keys) : undefined;
-    }
+  static getOldValue(changes: SimpleChanges, path: string): any {
+    let keys: string[];
+    let key: string;
+
+    [keys, key] = this.parsePath(path);
+    return (key && changes[key]) ? objectDeepParse(changes[key].previousValue, keys) : undefined;
   }
-
 
   /**
    * Function to parse currentValue from triggered by changes on ngOnChange
-   *
    * @param changes - SimpleChanges
    * @param path - string
    * @return lowest layer value or changes object itself when path cannot be parsed
    */
-  static getNewValue<T>(changes: SimpleChanges, path: string): T | undefined {
-    if (!path) {
-      return;
-    }
-    const [keys, key] = this.parsePath(path);
+
+  static getNewValue(changes: SimpleChanges, path: string): any {
+    let keys: string[];
+    let key: string;
+
+    [keys, key] = this.parsePath(path);
     return (key && changes[key]) ? objectDeepParse(changes[key].currentValue, keys) : undefined;
   }
 
-
   /**
    * Function to parse path to get keys for each layer
-   *
    * @param path - string
    * @return an array of two elements, one being an array of all the keys except first one, one being the first key
    */
+
   static parsePath(path: string): [string[], string] {
     const keys = path.split('.');
     let key = keys.shift();
@@ -55,5 +48,4 @@ export class NgChangeObjectValueParser {
     }
     return [keys, key];
   }
-
 }
