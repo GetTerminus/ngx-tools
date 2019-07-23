4cd57cf1a0689849f275b11f953eca748a3b24a9
Benjamin Charity <ben@benjamincharity.com>
2019-07-23 13:59:36 -0400

    chore(CI): update allowed commit scopes

--- tools/cz-config.js
+++ tools/cz-config.js
@@ -3,22 +3,56 @@
 module.exports = {
 
   types: [
-    {value: 'feat',     name: 'feat:     A new feature'},
-    {value: 'fix',      name: 'fix:      A bug fix'},
-    {value: 'docs',     name: 'docs:     Documentation only changes'},
-    {value: 'style',    name: 'style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)'},
-    {value: 'refactor', name: 'refactor: A code change that neither fixes a bug nor adds a feature'},
-    {value: 'perf',     name: 'perf:     A code change that improves performance'},
-    {value: 'test',     name: 'test:     Adding missing tests'},
-    {value: 'chore',    name: 'chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation'},
-    {value: 'revert',   name: 'revert:   Revert to a commit'},
-    {value: 'WIP',      name: 'WIP:      Work in progress'}
+    {
+      value: 'feat',
+      name: 'feat:     A new feature',
+    },
+    {
+      value: 'fix',
+      name: 'fix:      A bug fix',
+    },
+    {
+      value: 'docs',
+      name: 'docs:     Documentation only changes',
+    },
+    {
+      value: 'style',
+      name: 'style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)',
+    },
+    {
+      value: 'refactor',
+      name: 'refactor: A code change that neither fixes a bug nor adds a feature',
+    },
+    {
+      value: 'perf',
+      name: 'perf:     A code change that improves performance',
+    },
+    {
+      value: 'test',
+      name: 'test:     Adding missing tests',
+    },
+    {
+      value: 'chore',
+      name: 'chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation',
+    },
+    {
+      value: 'revert',
+      name: 'revert:   Revert to a commit',
+    },
+    {
+      value: 'WIP',
+      name: 'WIP:      Work in progress',
+    },
   ],
 
   scopes: [
+    {name: 'Browser'},
     {name: 'Coercion'},
+    {name: 'JWT'},
+    {name: 'Keycodes'},
     {name: 'Regex'},
     {name: 'Testing'},
+    {name: 'Type Guards'},
     {name: 'Utility'},
 
     {name: 'Build'},
@@ -29,6 +63,6 @@ module.exports = {
   ],
 
   allowCustomScopes: true,
-  allowBreakingChanges: ['feat', 'fix', 'refactor', 'revert', 'chore']
+  allowBreakingChanges: ['feat', 'fix', 'refactor', 'revert', 'chore'],
 
 };
