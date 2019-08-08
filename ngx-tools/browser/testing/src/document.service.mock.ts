95176f4f415c25b1f21ae478080b70d32376a3bf
Benjamin Charity <ben@benjamincharity.com>
2019-08-08 10:42:17 -0400

    fix(Browser): refactor to support better typings

--- ngx-tools/browser/testing/src/document.service.mock.ts
+++ ngx-tools/browser/testing/src/document.service.mock.ts
@@ -2,15 +2,17 @@ import { Injectable } from '@angular/core';
 import { noop } from '@terminus/ngx-tools/utilities';
 
 
+const document: unknown = {
+  body: {
+    createTextRange: noop,
+    appendChild: noop,
+  },
+  createElement: noop,
+  createRange: () => ({selectNodeContents: noop}),
+  execCommand: noop,
+};
+
 @Injectable()
 export class TsDocumentServiceMock {
-  public document = {
-    body: {
-      createTextRange: noop,
-      appendChild: noop,
-    },
-    createRange: () => ({selectNodeContents: noop}),
-    execCommand: noop,
-    createElement: noop,
-  };
+  public document = document as Document;
 }
